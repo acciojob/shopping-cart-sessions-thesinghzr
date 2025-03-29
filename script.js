a68007a443430528601b20b80c3778bcc94937f8
Suraj Singh <yousurajsingh@gmail.com>
2025-03-29 20:46:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-// This is the boilerplate code given for you
-// You can modify this code
-// Product data
+
 const products = [
   { id: 1, name: "Product 1", price: 10 },
   { id: 2, name: "Product 2", price: 20 },
@@ -8,7 +6,7 @@ const products = [
   { id: 4, name: "Product 4", price: 40 },
   { id: 5, name: "Product 5", price: 50 },
 ];
-
+let cart=[];
 // DOM elements
 const productList = document.getElementById("product-list");
 
@@ -16,23 +14,54 @@ const productList = document.getElementById("product-list");
 function renderProducts() {
   products.forEach((product) => {
     const li = document.createElement("li");
-    li.innerHTML = `${product.name} - $${product.price} <button class="add-to-cart-btn" data-id="${product.id}">Add to Cart</button>`;
+    li.innerHTML = `${product.name} - $${product.price} <button onclick="addToCart(${product.id})" class="add-to-cart-btn" data-id="${product.id}">Add to Cart</button>`;
     productList.appendChild(li);
   });
 }
+const cartlist=document.getElementById("cart-list");
 
 // Render cart list
-function renderCart() {}
+function renderCart() {
+	cartlist.innerHTML="";
+	cart.forEach((product,index)=>{
+		const li=document.createElement("li");
+		li.innerHTML=`${product.name} - $${product.price}
+		<button onclick="removeFromCart(${index})" class="remove-to-cart-btn" data-id="${product.id}">remove from Cart</button>`
+	    cartlist.appendChild(li);
+	});
+	savecart();
+}
 
 // Add item to cart
-function addToCart(productId) {}
+function addToCart(productId) {
+	const product=products.find((p)=>p.id===productId);
+	if(product){cart.push(product);
+	renderCart();}
+}
 
 // Remove item from cart
-function removeFromCart(productId) {}
+function removeFromCart(index) {
+	cart.splice(index,1);
+	renderCart();
+}
 
+function loadcart()
+{
+	const savedcart=sessionStorage.getItem("cart");
+	cart=savedcart?JSON.parse(savedcart):[];
+}
+function savecart()
+{
+	sessionStorage.setItem("cart",JSON.stringify(cart))
+}
 // Clear cart
-function clearCart() {}
+function clearCart() {
+cart.length=0;
+renderCart();
+	
+}
 
 // Initial render
+loadcart();
 renderProducts();
 renderCart();
